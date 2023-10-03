c7df00074385493385cb72f8b789e5283b073298
Alex <lehakri21@gmail.com>
2023-10-03 22:41:04 +0300

    page title

--- src/test/java/ConnectedShopHomePage.java
+++ src/test/java/ConnectedShopHomePage.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class ConnectedShopHomePage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class ConnectedShopHomePage {
+    private WebDriver driver;
+    @FindBy(xpath = "//title")
+    private WebElement title;
+    //подключаем конструктор
+    public ConnectedShopHomePage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+    public String getTitle() {
+        return title.getText();
+    }
+    public String getCurrentUrl() {
+        return driver.getCurrentUrl();
+    }
 }
--- src/test/java/ConnectedShopTest.java
+++ src/test/java/ConnectedShopTest.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class ConnectedShopTest {
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.Assert.assertEquals;
+
+public class ConnectedShopTest {
+    private WebDriver driver;
+    private ConnectedShopHomePage homePage;
+
+    @Before
+    public void setUp() {
+
+        driver = new ChromeDriver();
+        homePage = new ConnectedShopHomePage(driver);
+    }
+
+    @Test
+    public void testConnectedShopMainPage() {
+        driver.get("https://theconnectedshop.com");
+
+
+        String expectedTitle = "The Connected Shop - Smart Locks, Smart Sensors, Smart Home &amp; Office";
+        String actualTitle = homePage.getTitle();
+        assertEquals(expectedTitle, actualTitle);
+
+        String expectedUrl = "https://theconnectedshop.com/";
+        String actualUrl = homePage.getCurrentUrl();
+        assertEquals(expectedUrl, actualUrl);
+
+    }
+
+    @After
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }
